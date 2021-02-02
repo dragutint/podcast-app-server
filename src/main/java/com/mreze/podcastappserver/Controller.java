b40798375bb1f64e056af1a7524888a9d5368a33
Dragutin Todorovic <dragutin.todorovic@rgc.rs>
2021-02-02 22:30:49 +0100

    added button click example

--- src/main/java/com/mreze/podcastappserver/Controller.java
+++ src/main/java/com/mreze/podcastappserver/Controller.java
@@ -1,4 +1,17 @@
 package com.mreze.podcastappserver;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    @FXML
+    private Button btnAction1;
+
+    @FXML
+    void doSomething(ActionEvent event) {
+        System.out.println("click");
+    }
+
 }
